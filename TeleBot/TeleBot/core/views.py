7c751f6b7a38a9d89a7efd3329810c622e51f157
kka3009k <melis.babaev.su@gmail.com>
2020-06-05 17:24:59 +0600

    Create Parse Valute

--- TeleBot/TeleBot/core/views.py
+++ TeleBot/TeleBot/core/views.py
@@ -7,22 +7,24 @@ from django.utils.decorators import method_decorator
 from rest_framework.response import * 
 from rest_framework.decorators import api_view
 from rest_framework import status
-import telebot;
+from bs4 import BeautifulSoup
+#import telebot
 
 
-bot = telebot.TeleBot('1212419724:AAHgTJvXsv5njwJxv4-S_myvZOy95LxBFVg')
+#bot = telebot.TeleBot('1212419724:AAHgTJvXsv5njwJxv4-S_myvZOy95LxBFVg')
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+#@bot.message_handler(commands=['start'])
+#def start_message(message):
+#    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
-bot.polling()
+#bot.polling()
 
 
 @api_view(['GET', 'POST'])
 def send_bot_message(request):
     if request.method == 'GET':
+        get_and_parse()
         return Response(data="tests",status=status.HTTP_201_CREATED)
     elif request.method == 'POST':
         data = request.data
@@ -35,3 +37,27 @@ def send_bot_message(request):
             return Response(data="OK", status=status.HTTP_201_CREATED)
         except Exception as ex:
            return Response(data=str(ex), status=status.HTTP_400_BAD_REQUEST)
+
+
+def get_and_parse():
+    url='https://www.bankasia.kg/ru/'
+    site = requests.get(url)
+    site.encoding = 'utf8' 
+    with open('test.txt', 'w') as output_file:
+        output_file.write(site.text)
+    soup = BeautifulSoup(site.text, 'lxml')
+    head = soup.find('div', {'id': 'home'})
+    head = head.find('tbody')
+    heads = []
+    heads.append('Валюта: Покупка: Продажа: НБКР:')
+    k = 0
+    for i in head:
+       k=k+1
+       heads.append(str(i).replace('<tr>','').replace('</tr>','').replace('<td>','').replace('</td>','').replace('\n','   '))
+       print(k)
+    for i in heads:
+        print(i)
+
+   
+
+
